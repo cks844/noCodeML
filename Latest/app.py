b86dd6c383e5931a5eb02abdde07f7b67c456369
jayasankar-shyam <jayasankar06shyam@gmail.com>
2023-12-03 14:33:49 +0530

    prediction done

--- Latest/app.py
+++ Latest/app.py
@@ -2,19 +2,20 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import numpy as np
 import matplotlib
 matplotlib.use('agg')  # Use the 'agg' backend
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
-
+import ast
 app = Flask(__name__)
 
 # Helper function for linear regression
 def perform_linear_regression(file):
     df = pd.read_csv(file)
-    x = df.iloc[:,:-1].values.reshape(-1, 1)
-    y = df.iloc[:,0:-1].values.reshape(-1, 1)
+    x = df.iloc[:, :-1].values.reshape(-1, 1)
+    y = df.iloc[:, -1].values.reshape(-1, 1)
 
     # Perform Linear Regression
     model = LinearRegression()
@@ -34,7 +35,7 @@ def perform_linear_regression(file):
 
     plt.close()
 
-    return img_str, model
+    return model, img_str
 
 # Main route
 @app.route('/')
@@ -51,38 +52,31 @@ def upload():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
-    plot, model = perform_linear_regression(file)
+    model, plot = perform_linear_regression(file)
 
     return render_template('linearreg.html', plot=plot, model=model)
-
 
 # Predict route
 @app.route('/predict', methods=['POST'])
 def predict():
     new_value = float(request.form['new_value'])  # Get the new value as a float
-
-    # Get the model parameters as a string and convert it to a dictionary
-    model_params_str = request.form.get('model')
-    model_params = eval(model_params_str) if model_params_str else {}
-
     # Create a new instance of LinearRegression
     model = LinearRegression()
 
-    # Fit the model with dummy data to initialize its parameters
-    dummy_x = [[0]]
-    dummy_y = [0]
-    model.fit(dummy_x, dummy_y)
-
-    # Set the parameters for the model
-    model.set_params(**model_params)
-
+    # Set the parameters for the new instance
+    model.intercept_ = request.form['inter']
+    model.intercept_=float(model.intercept_.strip('[]'))
+    model.coef_ = request.form['coef']
+    outer_list = ast.literal_eval(model.coef_)
+    inner_list_floats = [float(item) for item in outer_list[0]]
+    model.coef_ = np.array([inner_list_floats])
     # Reshape the new value to fit the model's input requirements
-    new_value_reshaped = [[new_value]]
+    new_value_reshaped = np.array([new_value]).reshape(-1, 1)
 
     # Use the trained model to predict the new value
-    prediction = model.predict(new_value_reshaped)[0]
-
-    return render_template('linearreg.html', prediction=prediction, model=model.get_params())
+    prediction = model.predict(new_value_reshaped)
 
 
-app.run(debug=True, use_reloader=True,port=5004)
+    return render_template('linearreg.html', prediction=prediction, model=model.get_params(), new_value=new_value)
+
+app.run(debug=True, use_reloader=True, port=5004)
